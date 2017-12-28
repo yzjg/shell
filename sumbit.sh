e5a5ac0087dfd69ed3974934cdee4bb7177439a0
yzjg <785246661@qq.com>
2017-12-28 19:22:45 +0800

    Create sumbit.sh

---
+++ sumbit.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+keys=(144,399,673,752,,387,,485)
+for  k in   ${keys[@]};do
+s=`echo $k  |  cut  -f  1  -d  ","`
+e=`echo $k  | cut  -f  1  -d  ","`
+
+nohup  java  -jar     export.jar \
+$s   \
+$e  \
+>>  res.txt  2>&1
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
